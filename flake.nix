115d6fba6b2b4d1f41fd72c519fd49753584e5c1
Santiago C. Saavedra <zrock_git@zohomail.com>
2025-06-06 09:28:38 -0400

    chore(flake): development enviroment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Development environment with Node.js 20";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+          ];
+
+          shellHook = ''
+            echo "Node.js development environment ready!"
+            echo "Run 'npm run dev' to start your development server"
+          '';
+        };
+
+        packages.dev-script = pkgs.writeShellApplication {
+        name = "dev-script";
+        runtimeInputs = [ pkgs.nodejs_24 ];
+        text = ''
+        nohup npm run dev &
+        '';
+        };
+      }
+    );
+}
